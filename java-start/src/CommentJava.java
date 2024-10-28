e8c515313a9d1f35c418e1b60a514e3d3ee91843
ljh468 <ljh468@naver.com>
2024-10-28 17:56:54 +0900

    add java-start orientation

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+  public static void main(String[] args) {
+    System.out.println("hello java1"); // java1 출력 주석
+    //    System.out.println("hello java2"); // 주석은 무시
+
+    /*
+    System.out.println("hello java3"); // java3 출력 (주석 무시)
+    System.out.println("hello java4"); // java4 출력 (주석 무시)
+    */
+  }
+}

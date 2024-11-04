daf69f491e3efd139df4eb0aaed0aab5e651f965
ljh468 <ljh468@naver.com>
2024-11-05 00:38:04 +0900

    add: scanner practice

---
+++ java-start/src/scanner/ScannerWhile1.java
@@ -0,0 +1,25 @@
+package scanner;
+
+import java.util.Scanner;
+
+/**
+ * Scanner 반복 예제1<p>
+ * 사용자가 입력한 문자열을 그대로 출력하는 예제를 만들어보자. `exit` 라는 문자가 입력되면 프로그램을 종료한다.<p>
+ * 프로그램은 반복해서 실행된다.
+ */
+public class ScannerWhile1 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.print("문자열을 입력하세요 (\"exit\"를 입력하면 종료): ");
+      String str = scanner.nextLine();
+      if (str.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+      System.out.println("입력한 문자열: " + str);
+    }
+  }
+}

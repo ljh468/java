8aa4fac2c9d8bb3c45222279abd8d99dcbcb7774
ljh468 <ljh468@naver.com>
2024-11-06 17:55:27 +0900

    add: array practice

--- java-start/src/array/ArrayDi4.java
+++ java-start/src/array/ArrayDi4.java
@@ -3,9 +3,10 @@ package array;
 public class ArrayDi4 {
 
   public static void main(String[] args) {
-    // 2x3 2차원 배열을 만든다.
+    // 10x10 2차원 배열을 만든다.
     int[][] arr = new int[10][10];
 
+    // 순서대로 1씩 증가하는 값으로 초기화
     int i = 1;
     for (int row = 0; row < arr.length; row++) {
       for (int column = 0; column < arr[row].length; column++) {
@@ -13,6 +14,7 @@ public class ArrayDi4 {
       }
     }
 
+    // 2차원 배열 출력
     for (int row = 0; row < arr.length; row++) {
       for (int column = 0; column < arr[row].length; column++) {
         System.out.print(arr[row][column] + " ");
--- java-start/src/array/EnhancedFor1.java
+++ java-start/src/array/EnhancedFor1.java
@@ -7,14 +7,15 @@ public class EnhancedFor1 {
 
     // 일반 for문
     for (int i = 0; i < numbers.length; i++) {
-      int number = numbers[i];
-      System.out.println(number);
+      System.out.println(numbers[i]);
     }
+    System.out.println();
 
     // 향상된 for문, for-each문
     for (int number : numbers) {
       System.out.println(number);
     }
+    System.out.println();
 
     // for-each문을 사용할 수 없는 경우, 증가하는 index 값 필요
     for (int i = 0; i < numbers.length; i++) {

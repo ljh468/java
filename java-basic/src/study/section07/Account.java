ee2258d4fd9e2d6f4af9605aa906f70de1508bad
ljh468 <ljh468@naver.com>
2024-11-18 19:08:34 +0900

    feat: submit section07 mission

---
+++ java-basic/src/study/section07/Account.java
@@ -0,0 +1,32 @@
+package study.section07;
+
+public class Account {
+
+  private String accountId;
+  private User user;
+
+  public Account(String accountId, User user) {
+    this.accountId = accountId;
+    this.user = user;
+  }
+
+  // 계정 ID 가져오기
+  public String getAccountId() {
+    return accountId;
+  }
+
+  // User의 닉네임 가져오기
+  public String getNickname() {
+    return user.getName();
+  }
+
+  // User 이름 변경 요청
+  public void requestUserNameChange(String newName) {
+    user.changeName(newName);
+  }
+
+  // 비밀번호 변경 요청
+  public void requestPasswordChange(String oldPassword, String newPassword) {
+    user.changePassword(oldPassword, newPassword);
+  }
+}
---
+++ java-basic/src/study/section07/AccountMain.java
@@ -0,0 +1,35 @@
+package study.section07;
+
+public class AccountMain {
+
+  public static void main(String[] args) {
+    // User 생성
+    User user = new User("jaehoon", "password_123");
+
+    // Account 생성
+    Account account = new Account("noah123", user);
+
+    // User 정보 출력
+    System.out.println("사용자 아이디: " + account.getAccountId()); // public 메서드로 접근 가능
+    System.out.println("사용자 이름: " + account.getNickname()); // public 메서드로 접근 가능
+
+    // User 이름 변경
+    account.requestUserNameChange("noah");
+    System.out.println("변경된 사용자 이름: " + account.getNickname()); // public 메서드로 접근 가능
+    System.out.println();
+
+    // 비밀번호 변경 요청
+    try {
+      System.out.println("### 비밀번호 변경요청 ###");
+      account.requestPasswordChange("password_123", "New_Password123");
+      System.out.println("비밀번호가 성공적으로 변경되었습니다.");
+      System.out.println();
+
+      System.out.println("### 잘못된 비밀번호 변경요청 ###");
+      account.requestPasswordChange("Invalid_Password", "New_Password456");
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
+

3a3a7671d546bb768600236dc85a69f65e591ebe
ljh468 <ljh468@naver.com>
2024-11-19 11:58:24 +0900

    feat: Account deposit, withdraw implement

--- java-basic/src/study/section07/Account.java
+++ java-basic/src/study/section07/Account.java
@@ -1,32 +1,62 @@
 package study.section07;
 
+import java.util.UUID;
+
 public class Account {
 
-  private String accountId;
+  private UUID accountId;
   private User user;
+  private volatile int balance;
 
-  public Account(String accountId, User user) {
-    this.accountId = accountId;
-    this.user = user;
+  public Account(User user) {
+    this(user, 0);
   }
 
-  // 계정 ID 가져오기
-  public String getAccountId() {
+  public Account(User user, int balance) {
+    this.accountId = UUID.randomUUID();
+    this.user = user;
+    this.balance = balance;
+  }
+
+  protected int getBalance() {
+    return balance;
+  }
+
+  // 계좌 ID 가져오기
+  public UUID getAccountId() {
     return accountId;
   }
 
-  // User의 닉네임 가져오기
-  public String getNickname() {
-    return user.getName();
+  // 입금(deposit) 메서드
+  public void deposit(int amount) {
+    validateAmount(amount);
+    synchronized (Account.class) {
+      balance += amount;
+      System.out.println("입금이 정상적으로 처리되었습니다. 현재 잔액은 " + balance);
+    }
   }
 
-  // User 이름 변경 요청
-  public void requestUserNameChange(String newName) {
-    user.changeName(newName);
+  // 출금(withdraw) 메서드
+  public void withdraw(String password, int amount) {
+    validateAmount(amount);
+    synchronized (Account.class) {
+      if (amount > balance) {
+        throw new IllegalStateException("잔액이 부족합니다. 현재 잔액은 " + balance);
+      }
+      user.validatePassword(password);
+      balance -= amount;
+      System.out.println("출금이 정상적으로 처리되었습니다. 현재 잔액은 " + balance);
+    }
+  }
+
+  private void validateAmount(int amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("금액이 올바르지 않습니다. 다시 확인해주세요.");
+    }
   }
 
   // 비밀번호 변경 요청
-  public void requestPasswordChange(String oldPassword, String newPassword) {
-    user.changePassword(oldPassword, newPassword);
+  public void requestPasswordChange(String password, String newPassword) {
+    user.changePassword(password, newPassword);
   }
 }
--- java-basic/src/study/section07/User.java
+++ java-basic/src/study/section07/User.java
@@ -1,7 +1,5 @@
 package study.section07;
 
-import static java.util.Objects.isNull;
-
 public class User {
 
   private String name;
@@ -17,29 +15,20 @@ public class User {
   }
 
   public void changeName(String newName) {
-    if (isNull(newName) || newName.isBlank()) {
-      throw new IllegalArgumentException("이름은 비어 있거나 공백이 을 수 없습니다.");
-    }
     this.name = newName;
     System.out.println("이름이 성공적으로 변경되었습니다.");
   }
 
-  public void changePassword(String oldPassword, String newPassword) {
-    validateOldPassword(oldPassword);
-    validateNewPassword(newPassword);
+  public void changePassword(String password, String newPassword) {
+    validatePassword(password);
     this.password = newPassword;
     System.out.println("비밀번호가 성공적으로 변경되었습니다.");
   }
 
-  private void validateOldPassword(String oldPassword) {
+  public void validatePassword(String oldPassword) {
     if (!password.equalsIgnoreCase(oldPassword)) {
       throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
     }
   }
 
-  private void validateNewPassword(String newPassword) {
-    if (isNull(newPassword) || newPassword.isBlank()) {
-      throw new IllegalArgumentException("새 비밀번호는 비어 있거나 공백일 수 없습니다.");
-    }
-  }
 }

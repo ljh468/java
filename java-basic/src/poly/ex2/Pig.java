6a4945a52359a58518367302b2303dfc3c3ada88
ljh468 <ljh468@naver.com>
2024-11-23 03:06:27 +0900

    feat: with polymorphism2 practice

---
+++ java-basic/src/poly/ex2/Pig.java
@@ -0,0 +1,5 @@
+package poly.ex2;
+
+public class Pig extends Animal {
+  //TODO: sound 해야 하는데...
+}
---
+++ java-basic/src/poly/ex3/AbstractAnimal.java
@@ -0,0 +1,14 @@
+package poly.ex3;
+
+// 추상 클래스
+public abstract class AbstractAnimal {
+
+  // 추상 메서드
+  public abstract void sound();
+
+  // 일반 메서드
+  public void move() {
+    System.out.println("동물이 움직입니다.");
+  }
+
+}

270aba8fa80718652c7009c6077e1bea29029429
ljh468 <ljh468@naver.com>
2025-02-07 23:14:03 +0900

    feat: submit javaStudy section03 mission

---
+++ java-mid2/src/study/section03/Goods.java
@@ -0,0 +1,8 @@
+package study.section03;
+
+public class Goods extends Item {
+
+  public Goods(String name, int price) {
+    super(name, price);
+  }
+}
---
+++ java-mid2/src/study/section03/Order.java
@@ -0,0 +1,3 @@
+package study.section03;
+
+public record Order(int id) {}

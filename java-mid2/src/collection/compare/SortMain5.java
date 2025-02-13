af0d1984594796199189311827f52c73603dc16e
ljh468 <ljh468@naver.com>
2025-02-14 04:30:01 +0900

    feat: section11 sort(Comparator, Comparable) practice

---
+++ java-mid2/src/collection/compare/SortMain5.java
@@ -0,0 +1,31 @@
+package collection.compare;
+
+import java.util.TreeSet;
+
+public class SortMain5 {
+
+  public static void main(String[] args) {
+    MyUser myUser1 = new MyUser("a", 30);
+    MyUser myUser2 = new MyUser("b", 20);
+    MyUser myUser3 = new MyUser("c", 10);
+
+    TreeSet<MyUser> treeSet1 = new TreeSet<>();
+    treeSet1.add(myUser1);
+    treeSet1.add(myUser2);
+    treeSet1.add(myUser3);
+    System.out.println("Comparable 기본 정렬");
+    System.out.println(treeSet1);
+
+    // 람다 or 메서드 참조를 이용해서 생성
+    // TreeSet<MyUser> treeSet2 = new TreeSet<>((o1, o2) -> o1.getId().compareTo(o2.getId()));
+    // Set<MyUser> treeSet2 = new TreeSet<>(Comparator.comparing(MyUser::getId));
+
+    TreeSet<MyUser> treeSet2 = new TreeSet<>(new IdComparator());
+    treeSet2.add(myUser1);
+    treeSet2.add(myUser2);
+    treeSet2.add(myUser3);
+    System.out.println("IdComparator 정렬");
+    System.out.println(treeSet2);
+  }
+
+}

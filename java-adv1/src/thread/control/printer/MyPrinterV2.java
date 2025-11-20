672c745a21a68d1fb0dd37a917359b7209383917
ljh468 <ljh468@naver.com>
2025-11-21 00:54:15 +0900

    add. 프린터 만들어보기 (쓰레드 활용)

---
+++ java-adv1/src/thread/control/printer/MyPrinterV2.java
@@ -0,0 +1,71 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+
+public class MyPrinterV2 {
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer();
+
+        Thread thread = new Thread(printer, "printer");
+        thread.start();
+
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("프린터할 문서를 입력하세요. 강제종료 (q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printer.work = false;
+                thread.interrupt(); // interrupt() 즉시 종료
+                break;
+            }
+
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+
+        volatile boolean work = true;
+
+        // 여러 쓰레드가 사용하는 큐 사용: ConcurrentLinkedQueue
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+
+            while (work) {
+
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                try {
+                    String job = jobQueue.poll();
+
+                    log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료: " + job);
+
+                } catch (InterruptedException e) {
+                    log("즉시");
+                    break;
+                }
+            }
+
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+
+    }
+}

09e6ad5c771d6953f9e45b9ea2793745dc74cbed
ljh468 <ljh468@naver.com>
2025-11-26 00:32:10 +0900

    add. volatile 키워드 활용

---
+++ java-adv1/src/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,40 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+
+    }
+
+    static class MyTask implements Runnable {
+
+        boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+
+            while (runFlag) {
+                // runFlag가 false로 변하면 탈출
+            }
+
+            // ########### 종료가 되지 않음 ###########
+            log("task 종료");
+
+        }
+    }
+}

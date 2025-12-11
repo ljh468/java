b423e8ce3715f040d6a36c8cb5472c16f1daa135
ljh468 <ljh468@naver.com>
2025-12-12 00:43:46 +0900

    add. 생상자 소비자 문제 예제 2 분석

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -11,14 +11,15 @@ public class BoundedMain {
     public static void main(String[] args) {
 
         // 1. BoundedQueue 선택
-        BoundedQueueV1 queue = new BoundedQueueV1(2);
+        // BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         // 2. 생산자, 소비자 실행 순서 선택 (반드시 하나만 선택)
         producerFirst(queue); // 생산자 먼저 실행
         // consumerFirst(queue); // 소비자 먼저 실행
     }
 
-    private static void producerFirst(BoundedQueueV1 queue) {
+    private static void producerFirst(BoundedQueue queue) {
         log("== [생산자 먼저 실행] 시작, " + queue.getClass().getSimpleName() + " ==");
 
         List<Thread> threads = new ArrayList<>();
